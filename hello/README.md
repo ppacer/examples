219c56922cfef9bfa38180a0c0d42b0479ca286b
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-12-09 22:51:07 +0100

    Setup hello DAG example

---
+++ hello/README.md
@@ -0,0 +1,20 @@
+# Hello World example
+
+The only requirements to run hello example program is having Go in version
+`>=1.21` and SQlite. To compile and run the example just run:
+
+```
+go build
+./hello
+```
+
+Currently there is only backend side - scheduler, executor and database. If you
+want to checkout details on your DAG runs, please explore `scheduler.db` SQLite
+database. Few examples:
+
+```
+sqlite3 scheduler.db 'SELECT DagId, StartTs, Schedule, CreateTs FROM dags'
+sqlite3 scheduler.db 'SELECT DagId, TaskId, IsCurrent, InsertTs, TaskTypeName FROM dagtasks'
+sqlite3 scheduler.db 'SELECT * FROM dagruns'
+sqlite3 scheduler.db 'SELECT * FROM dagruntasks'
+```
---
+++ hello/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/dskrzypiec/scheduler/dag"
+	"github.com/dskrzypiec/scheduler/db"
+	"github.com/dskrzypiec/scheduler/exec"
+	"github.com/dskrzypiec/scheduler/sched"
+)
+
+type EmptyTask struct {
+	TaskId string
+}
+
+func (et EmptyTask) Id() string { return et.TaskId }
+func (et EmptyTask) Execute() {
+	fmt.Printf(" ========== EmptyTask: %s ==========\n", et.TaskId)
+}
+
+func setupDAGs() {
+	//        t2 ------
+	//      /          \
+	// root              end
+	//      \          /
+	//        t3 --> t4
+	root := dag.Node{Task: EmptyTask{TaskId: "Start"}}
+	t2 := dag.Node{Task: EmptyTask{TaskId: "t2"}}
+	t3 := dag.Node{Task: EmptyTask{TaskId: "t3"}}
+	t4 := dag.Node{Task: EmptyTask{TaskId: "t4"}}
+	end := dag.Node{Task: EmptyTask{TaskId: "Finish"}}
+	root.Next(&t2)
+	root.Next(&t3)
+	t3.Next(&t4)
+	t2.Next(&end)
+	t4.Next(&end)
+
+	startTs := time.Date(2023, time.December, 8, 12, 0, 0, 0, time.UTC)
+	schedule := dag.FixedSchedule{Interval: 5 * time.Second, Start: startTs}
+	emptyDag := dag.New(dag.Id("empty_dag")).
+		AddSchedule(&schedule).
+		AddRoot(&root).
+		Done()
+
+	// Add emptyDag to the central DAG repository.
+	dag.Add(emptyDag)
+}
+
+func main() {
+	const port = 8080
+	setupDAGs()
+
+	dbClient, dbErr := db.NewSqliteClient("scheduler.db")
+	if dbErr != nil {
+		log.Panic(dbErr)
+	}
+	scheduler := sched.New(dbClient)
+	schedulerHttpHandler := scheduler.Start()
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: schedulerHttpHandler,
+	}
+
+	// Run executor within the same program
+	go func() {
+		executor := exec.New(fmt.Sprintf("http://localhost:%d", port), nil)
+		executor.Start()
+	}()
+
+	lasErr := server.ListenAndServe()
+	if lasErr != nil {
+		slog.Error("ListenAndServer failed", "err", lasErr)
+		log.Panic("Cannot start the server")
+	}
+}
